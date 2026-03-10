4cf17ac89796de1dba9883bee48a6ebb613914c3
jseto <jsetojseto@gmail.com>
2026-03-10 22:14:39 +0700

    fix: update document change listener memory allocation and region in FirebaseAdminDatasource

--- src/store/firebase-admin-datasource.ts
+++ src/store/firebase-admin-datasource.ts
@@ -124,7 +124,7 @@ export class FirebaseAdminDatasource extends DataSource {
 	}
 
 	protected override subscribeToDocumentChangeListener( collectionPathToListen: string, listener: DocumentChangeListener<DocumentObject> ): DocumentChangeListenerHandler | undefined {
-		const handler = functions.firestore.onDocumentUpdated({ document: collectionPathToListen + '/{docId}', memory: '512MiB' }, event => {
+		const handler = functions.firestore.onDocumentUpdated({ document: collectionPathToListen + '/{docId}', memory: '1GiB', region: 'europe-west2' }, event => {
 			const snapshot = event.data
 			listener({ 
 				before: snapshot?.before.data() as DocumentObject, 
